65ce570bac9d66b7e7ab649eda798430d12b952c
rasoini <l.rasoini@studenti.unipi.it>
2020-09-18 10:19:55 +0200

    Add Lab_1

---
+++ Lab_1/calculator.py
@@ -0,0 +1,29 @@
+#calculator.py
+
+def sum(m, n):
+	#TODO
+	res = m
+	if (n > 0):
+		while n > 0:
+			res = res + 1
+			n = n - 1
+	else:
+		while n < 0:
+			res = res - 1
+			n = n + 1
+	return res
+
+
+def divide(m, n):
+	res = 0
+	mabs = abs(m)
+	nabs = abs(n)
+	while mabs >= nabs:
+		res = res + 1
+		mabs = mabs - nabs
+	if (m < 0 and n < 0) or (m > 0 and n > 0) :
+		return res
+	else:
+		return -1 * res
+
+
